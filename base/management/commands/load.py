b2ac440da66e7a5b847c2040a194bb655b750382
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-15 18:20:17 +0200

    fixed syntax

--- base/management/commands/load.py
+++ base/management/commands/load.py
@@ -9,12 +9,11 @@ from wagtail.models import Site, Page
 
 
 class Command(BaseCommand):
-    """def _copy_files(self, local_storage, path):
-        """
+    def _copy_files(self, local_storage, path):
         """Recursively copy files from local_storage to default_storage. Used
         to automatically bootstrap the media directory (both locally and on
         cloud providers) with the images linked from the initial data (and
-        included in MEDIA_ROOT)."""
+        included in MEDIA_ROOT).
         """
         directories, file_names = local_storage.listdir(path)
         for directory in directories:
@@ -22,22 +21,23 @@ class Command(BaseCommand):
         for file_name in file_names:
             with local_storage.open(path + file_name) as file_:
                 default_storage.save(path + file_name, file_)
-"""
 
     def handle(self, **options):
-        fixtures_dir = os.path.join(settings.PROJECT_DIR, settings.NAME, 'management', 'fixtures')
+        fixtures_dir = os.path.join(
+            settings.PROJECT_DIR, settings.NAME, 'management', 'fixtures')
         fixture_file = os.path.join(fixtures_dir, 'seed.json')
 
         print("Copying media files to configured storage...")
-        #local_storage = FileSystemStorage(os.path.join(fixtures_dir, 'media'))
-        #self._copy_files(local_storage, '')  # file storage paths are relative
+        local_storage = FileSystemStorage(os.path.join(fixtures_dir, 'media'))
+        self._copy_files(local_storage, '')  # file storage paths are relative
 
         # Wagtail creates default Site and Page instances during install, but we already have
         # them in the data load. Remove the auto-generated ones.
         if Site.objects.filter(hostname='localhost').exists():
             Site.objects.get(hostname='localhost').delete()
         if Page.objects.filter(title='Welcome to your new Wagtail site!').exists():
-            Page.objects.get(title='Welcome to your new Wagtail site!').delete()
+            Page.objects.get(
+                title='Welcome to your new Wagtail site!').delete()
 
         call_command('loaddata', fixture_file, verbosity=0)
         call_command('update_index', verbosity=0)
