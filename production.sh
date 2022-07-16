3cc45de4c4bf1c05b8d973bf186b05f59a310d53
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-16 14:25:17 +0200

    fixed production set up

--- production.sh
+++ production.sh
@@ -8,7 +8,10 @@ INTEXT_OCCURRENCES="s/wagtailitalia"/"${NAME}"/"g"
 DOMAIN_OCCURRENCES="s/wagtail-italia.it"/"${DOMAIN}"/"g"
 
 # Renaming project variables so that they match the new project name
-find . -iname "wagtailitalia*" | rename $WI_OCCURRENCES && rename $WI_OCCURRENCES
+find . -iname "wagtailitalia" | rename $WI_OCCURRENCES
+find . -iname "wagtailitalia*" | rename $WI_OCCURRENCES
+find . -iname "wagtailitalia*" | rename $WI_OCCURRENCES
+
 find . -iname "wagtail-italia.it" | rename $DOMAIN_OCCURRENCES
 grep -RiIl "wagtailitalia" | xargs sed -i $INTEXT_OCCURRENCES
 grep -RiIl "wagtail-italia.it" | xargs sed -i $DOMAIN_OCCURRENCES
