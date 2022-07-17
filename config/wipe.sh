db26f67eb96eb121e627bea1fc9eb3b8914e0bd1
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-17 18:00:09 +0200

    improved

--- config/wipe.sh
+++ config/wipe.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
 NAME=$1
 DOMAIN=$2
-PASSWORD= source .secret
 
+sudo supervisorctl stop $NAME
+sudo supervisorctl remove $NAME
+sudo rm /etc/supervisor/conf.d/${NAME}.conf
+sudo rm -rf /home/admin/${DOMAIN}
+sudo rm -rf /home/admin/logs/${NAME}
 
-rm -rf "../${DOMAIN}"
-$echo $PASSWORD | sudo -u postgres psql -c 'DROP DATABASE ${NAME};'
+psql -c 'DROP DATABASE '$NAME';'
+sudo rm /etc/nginx/sites-enabled/${DOMAIN}
+sudo rm /etc/nginx/sites-available/${DOMAIN}
 
+sudo systemctl restart nginx
