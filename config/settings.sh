f088f67f4ec1efd54bedf79bb5d9f46341ca562a
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-18 09:59:46 +0200

    Fixed bugs

--- config/settings.sh
+++ config/settings.sh
@@ -5,14 +5,14 @@ DOMAIN=$2
 USER=$3
 
 ## Supervisor configuration
-cp ./config/"${NAME}".conf /etc/supervisor/conf.d/"${NAME}".conf
+cp ./config/wagtailitalia.conf /etc/supervisor/conf.d/"${NAME}".conf
 
 ## Gunicorn configuration
 cp ./config/gunicorn_start /home/"${USER}"/"${DOMAIN}"/"${NAME}"-env/bin/gunicorn_start
 sudo chmod g+wx /home/"${USER}"/"${DOMAIN}"/"${NAME}"-env/bin/gunicorn_start
 
 ## Copy Nginx configuration
-cp ./config/nginx_nocert/"${DOMAIN}" /etc/nginx/sites-available/"${DOMAIN}"
+cp ./config/nginx_nocert/wagtail-italia.it /etc/nginx/sites-available/"${DOMAIN}"
 
 # Set up manage.py to use production settings
 cp ./config/manage.py ./manage.py
@@ -20,4 +20,4 @@ cp ./config/manage.py ./manage.py
 # Create symbolic link for domain, if it does not exist
 ln -s /etc/nginx/sites-available/"${DOMAIN}" /etc/nginx/sites-enabled/
 
-
+logout
