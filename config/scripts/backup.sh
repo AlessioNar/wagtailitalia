05f085c2e3b32da96ad9e912ac4f9ce170c6046a
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-18 17:16:17 +0200

    cleaned up repository

---
+++ config/scripts/backup.sh
@@ -0,0 +1,2 @@
+ssh admin@139.162.184.175 'pg_dump -F t hopeheatwaveseu > /home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar'
+scp admin@139.162.184.175:/home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu/hopeheatwaveseu.tar
