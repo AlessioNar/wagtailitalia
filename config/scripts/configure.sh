765ae4eb3e9aeb8163d85f21e5700257449e9440
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-18 12:53:25 +0200

    again with supervisor

--- config/scripts/configure.sh
+++ config/scripts/configure.sh
@@ -5,12 +5,15 @@ DOMAIN=$2
 USER=$3
 
 ## Gunicorn configuration
-cp ./config/gunicorn/gunicorn_start /home/"${USER}"/"${DOMAIN}"/"${NAME}"-env/bin/gunicorn_start
-sudo chmod g+wx /home/"${USER}"/"${DOMAIN}"/"${NAME}"-env/bin/gunicorn_start
+#cp ./config/gunicorn/gunicorn_start /home/"${USER}"/"${DOMAIN}"/"${NAME}"-env/bin/gunicorn_start
+#sudo chmod g+wx /home/"${USER}"/"${DOMAIN}"/"${NAME}"-env/bin/gunicorn_start
 
 # Setting up Systemd configuration 
-sudo cp ./config/systemd/gunicorn.service /etc/systemd/system/gunicorn.service
-sudo cp ./config/systemd/gunicorn.socket /etc/systemd/system/gunicorn.socket
+#sudo cp ./config/systemd/gunicorn.service /etc/systemd/system/gunicorn.service
+#sudo cp ./config/systemd/gunicorn.socket /etc/systemd/system/gunicorn.socket
+
+# Supervisor configuration
+sudo cp ./config/supervisor/wagtailitalia.conf /etc/supervisor/conf.d/"${NAME}".conf
 
 ## Copy Nginx configuration
 cp ./config/nginx/nginx_nocert/wagtail-italia.it /etc/nginx/sites-available/"${DOMAIN}"
