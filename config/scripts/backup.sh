46c476f269e71e5e6b0e667a8742fde038c77e07
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-19 09:53:46 +0200

    Implemented backup command

--- config/scripts/backup.sh
+++ config/scripts/backup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+USER=$1
+IP_ADDRESS=$2
+NAME=$3
+DOMAIN=$4
 
 # Dump database 
-ssh admin@139.162.184.175 'pg_dump -F t hopeheatwaveseu > /home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar'
+ssh $USER@$IP_ADDRESS 'pg_dump -F t hopeheatwaveseu > /home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar'
 echo "Dump database done"
 
 # Copy database dump to backup server
-scp admin@139.162.184.175:/home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu/hopeheatwaveseu.tar
+scp $USER@$IP_ADDRESS:/home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu/hopeheatwaveseu.tar
 echo "Copy database dump to backup server done"
 
 # Copy media files to backup server
-scp -r admin@139.162.184.175:/home/admin/hope-heatwaves.eu/media /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu
+scp -r $USER@$IP_ADDRESS:/home/admin/hope-heatwaves.eu/media /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu
 echo "Copy media files to backup server done"
 
