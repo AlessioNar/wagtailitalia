a885254bb4803a9756d179ae17c0f82bf364ec27
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-20 10:00:10 +0200

    Fixed backupping

--- config/scripts/backup.sh
+++ config/scripts/backup.sh
@@ -7,12 +7,13 @@ DOMAIN=$4
 # Dump database 
 ssh $USER@$IP_ADDRESS 'pg_dump -F t hopeheatwaveseu > /home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar'
 echo "Dump database done"
+mkdir /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu/backup
 
 # Copy database dump to backup server
-scp $USER@$IP_ADDRESS:/home/admin/hope-heatwaves.eu/hopeheatwaveseu.tar /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu/hopeheatwaveseu.tar
+scp $USER@$IP_ADDRESS:/home/admin/$DOMAIN/hopeheatwaveseu.tar /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu/backup
 echo "Copy database dump to backup server done"
 
 # Copy media files to backup server
-scp -r $USER@$IP_ADDRESS:/home/admin/hope-heatwaves.eu/media /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu
+scp -r $USER@$IP_ADDRESS:/home/admin/hope-heatwaves.eu/media /home/prometeo/Desktop/Developer/ensa-network.eu/hopeheatwaveseu/backup/media
 echo "Copy media files to backup server done"
 
