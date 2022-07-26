168831490da8b05fb0e9ed4edac8b9e2422405dd
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-26 17:28:55 +0200

    Improved the branding

--- wagtailitalia/settings/base.py
+++ wagtailitalia/settings/base.py
@@ -194,7 +194,7 @@ MEDIA_URL = '/media/'
 
 # Wagtail settings
 # @todo this may break
-WAGTAIL_SITE_NAME = os.getenv('WEBSITE_NAME')
+WAGTAIL_SITE_NAME = os.getenv('DOMAIN')
 
 # Search
 # https://docs.wagtail.org/en/stable/topics/search/backends.html
@@ -206,7 +206,7 @@ WAGTAILSEARCH_BACKENDS = {
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = os.getenv('WEBSITE_NAME')
+BASE_URL = os.getenv('DOMAIN')
 WAGTAILADMIN_BASE_URL = os.getenv('DOMAIN')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
