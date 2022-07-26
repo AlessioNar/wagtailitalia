8af72a1b985efac8d06dd0ea92050fb7b504a095
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-26 15:09:18 +0200

    Added contact form functionality

--- contact/models.py
+++ contact/models.py
@@ -1,15 +1,35 @@
-"""
 from django.db import models
+from modelcluster.fields import ParentalKey
 
-# Create your models here.
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
+from wagtail.core.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
+
 class FormField(AbstractFormField):
 	
-	page = ParentalKey('FormPage', on_delete=models.CASCADE,
+	page = ParentalKey('ContactPage',
+		on_delete=models.CASCADE,
 		related_name='form_fields',
-
 		)
 
 class ContactPage(AbstractEmailForm):
 
-	template = "contact/contact_page.html"
-"""
+	template = 'contact/contact_page.html'
+
+	intro = RichTextField(blank=True)
+	thank_you_text = RichTextField(blank=True)
+
+	content_panels = AbstractEmailForm.content_panels + [
+					FieldPanel('intro'),
+					InlinePanel('form_fields', label="Form fields"),
+					FieldPanel('thank_you_text'),
+					MultiFieldPanel([
+						FieldRowPanel([
+							FieldPanel('from_address', classname="col6"),
+							FieldPanel('to_address', classname="col6"),
+						]),
+						FieldPanel('subject'),
+					], "Email settings"),					
+				]
+		
