da0afe0fdfc8ef0de9c12219ec47b884c844cd5e
Alessio Nardin <alessio.nardin@gmail.com>
2022-07-28 13:29:42 +0200

    implemented the big part of cards

--- themes/models.py
+++ themes/models.py
@@ -31,6 +31,13 @@ register_snippet(Color)
 class Theme(BaseSetting):
     """Wrapper class to provide the other setting classes with some common methods"""
     
+    ALIGN_FIELDS = [
+        ('center', 'center'),
+        ('left', 'left'),
+        ('right', 'right'),        
+    ]
+
+    # Color fields
     primary = models.ForeignKey(Color, related_name="primary", null=True, on_delete=models.SET_NULL)    
     secondary = models.ForeignKey(Color, related_name="secondary", null=True, on_delete=models.SET_NULL)    
     dark = models.ForeignKey(Color, related_name="dark", null=True, on_delete=models.SET_NULL)    
@@ -39,32 +46,46 @@ class Theme(BaseSetting):
     success = models.ForeignKey(Color, related_name="success", null=True, on_delete=models.SET_NULL)    
     warning = models.ForeignKey(Color, related_name="warning", null=True, on_delete=models.SET_NULL)
 
+    # Font fields
     font_color = models.ForeignKey(Color, related_name="font_color", null=True, on_delete=models.SET_NULL)    
     font_size = models.IntegerField(default=16, help_text="Font size in px")
     font_family = models.CharField(max_length=255, default="Montserrat", 
         help_text="Choose a Google font family")        
 
+    # Navbar fields
     nav_title_size = models.IntegerField(default=16, help_text="Font size in px")
     nav_bg_color = models.ForeignKey('themes.Color', related_name="navbar_bg_color", on_delete=models.SET_NULL, null=True, blank=True)
     nav_text_color = models.ForeignKey('themes.Color', related_name="navbar_text_color", on_delete=models.SET_NULL, null=True, blank=True)
     nav_subtitle_size = models.IntegerField(default=11, help_text="Font size in px")
     nav_subtitle_show = models.BooleanField(default=True)
-    dark_background_color = models.BooleanField(default=False)
+    dark_background_color = models.BooleanField(default=False)    
 
+    # Carousel fields
     car_title_size = models.IntegerField(default=16, help_text="Font size in px")
     car_bg_color = models.ForeignKey('themes.Color', related_name="car_bg_color", on_delete=models.SET_NULL, null=True, blank=True)
     
+    # Card fields
+    card_title_size = models.IntegerField(default=16, help_text="Font size in px")
+    card_bg_color = models.ForeignKey('themes.Color', related_name="card_bg_color", on_delete=models.SET_NULL, null=True, blank=True)
+    card_text_color = models.ForeignKey('themes.Color', related_name="card_text_color", on_delete=models.SET_NULL, null=True, blank=True)
+    card_height = models.IntegerField(default=20, help_text="Card height in em")
+    card_text_align = models.CharField(max_length=10, choices=ALIGN_FIELDS, default='center')
+    card_image_height = models.IntegerField(default=15, help_text="Card height in em")
 
-    foot_bg_color = models.ForeignKey('themes.Color', related_name="footer_bg_color", on_delete=models.SET_NULL, null=True, blank=True)
-    foot_text_color = models.ForeignKey('themes.Color', related_name="footer_text_color", on_delete=models.SET_NULL, null=True, blank=True)
-    foot_text_size = models.IntegerField(default=16, help_text="Font size in px")    
-
+    # Jumbotron fields
     jumbo_bg_color = models.ForeignKey('themes.Color', related_name="jumbotron_bg_color", on_delete=models.SET_NULL, null=True, blank=True)
     jumbo_text_color = models.ForeignKey('themes.Color', related_name="jumbotron_text_color", on_delete=models.SET_NULL, null=True, blank=True)
     jumbo_title_size = models.IntegerField(default=16, help_text="Font size in px")
     jumbo_button_color = models.ForeignKey('themes.Color', on_delete=models.SET_NULL, null=True, blank=True)
     jumbo_button_size = models.IntegerField(default=14, help_text="Button size in rem")
-        
+
+    # Footer fields
+    foot_bg_color = models.ForeignKey('themes.Color', related_name="footer_bg_color", on_delete=models.SET_NULL, null=True, blank=True)
+    foot_text_color = models.ForeignKey('themes.Color', related_name="footer_text_color", on_delete=models.SET_NULL, null=True, blank=True)
+    foot_text_size = models.IntegerField(default=16, help_text="Font size in px")    
+
+    # Custom CSS field
+    css = models.TextField(blank=True, help_text="Custom CSS")    
    
     panels = [        
         MultiFieldPanel([
@@ -98,6 +119,15 @@ class Theme(BaseSetting):
         ], heading="Carousel"),
 
         MultiFieldPanel([
+            FieldPanel('card_title_size'),
+            FieldPanel('card_bg_color'),
+            FieldPanel('card_text_color'),
+            FieldPanel('card_height'),
+            FieldPanel('card_text_align'),
+            FieldPanel('card_image_height'),
+        ], heading="Card"),
+
+        MultiFieldPanel([
             FieldPanel('jumbo_bg_color'),
             FieldPanel('jumbo_text_color'),
             FieldPanel('jumbo_title_size'),
@@ -111,6 +141,10 @@ class Theme(BaseSetting):
             FieldPanel('foot_text_size'),
         ], heading="Footer"),
         
+        MultiFieldPanel([
+            FieldPanel('css'),
+        ], heading="Custom CSS"),
+
         ]
 
     class Meta:                
@@ -122,6 +156,9 @@ class Theme(BaseSetting):
         NAME = settings.NAME
         with open(os.path.join(NAME, "static/scss/themes/theme.scss"), "w") as f:
             
+            # Writing variables
+
+            ## Color variables
             f.write("$primary: " + self.primary.code + ";\n")            
             f.write("$secondary: " + self.secondary.code + ";\n")            
             f.write("$success: " + self.success.code + ";\n")            
@@ -129,11 +166,13 @@ class Theme(BaseSetting):
             f.write("$danger: " + self.danger.code + ";\n")
             f.write("$light: " + self.light.code + ";\n")
             f.write("$dark: " + self.dark.code + ";\n")            
-
+            
+            ## Font variables
             f.write("$font-family-sans-serif: " + self.font_family + ";\n")            
             f.write("$font-size-base: " + str(self.font_size * 0.0625) + "rem ;\n")
             f.write("$font-color: " + self.font_color.code + ";\n")
             
+            ## Navbar variables
             f.write("$navbar-brand-font-size: " + str(self.nav_title_size * 0.0625) + "rem ;\n")
             f.write("$navbar-subtitle-size: " + str(self.nav_subtitle_size * 0.0625) + "rem ;\n")
             f.write("$navbar-bg-color: " + self.nav_bg_color.code + ";\n")
@@ -142,9 +181,23 @@ class Theme(BaseSetting):
             f.write("$navbar-text-color: " + self.navbar.text_color + ";\n")            
             f.write("$navbar-subtitle-show: " + self.navbar.subtitle_show + ";\n")
             """
+            
+            ## Carousel variables
             f.write("$carousel-bg-color: " + self.car_bg_color.code + ";\n")
             f.write("$carousel-font-size: " + str(self.car_title_size * 0.0625) + "rem;\n")
             
+            ## Card variables
+            f.write("$card-bg-color: " + self.card_bg_color.code + ";\n")
+            f.write("$card-height: " + str(self.card_height) + "em;\n")            
+            f.write("$card-img-height: " + str(self.card_image_height) + "em;\n")
+
+            f.write("$card-font-size: " + str(self.card_title_size * 0.0625) + "rem;\n")
+            f.write("$card-text-align: " + self.card_text_align + ";\n")
+            f.write("$card-font-color: " + self.card_text_color.code + ";\n")
+            
+            
+
+            ## Jumbotron variables
             f.write("$jumbotron-bg-color: " + self.jumbo_bg_color.code + ";\n")
             f.write("$jumbotron-text-color: " + self.jumbo_text_color.code + ";\n")
             f.write("$jumbotron-button-color: " + self.jumbo_button_color.code + ";\n")
@@ -159,25 +212,40 @@ class Theme(BaseSetting):
             
             f.write("$jumbotron-button-size: " + self.jumbotron.button_size + ";\n")
             """
-
+            
+            # Import bootswatch and bootstrap scss
             f.write("@import 'wagtailitalia/static/scss/bootswatch/_variables';\n")
             f.write("@import 'wagtailitalia/static/scss/bootstrap/bootstrap';\n")
             f.write("@import 'wagtailitalia/static/scss/bootswatch/bootswatch';\n")
 
+            # Set the theme variables as scss
+            ## Navbar
             f.write(".navbar-brand-subtitle{\nfont-size: $navbar-subtitle-size\n}\n")
             f.write(".navbar{\nbackground-color: $navbar-bg-color\n}\n")
             f.write(".dropdown-menu{\nbackground-color: $navbar-bg-color\n}\n")      
+            
+            ## Carousel
             f.write(".carousel-caption {\nposition: inherit;\n}\n")
             f.write(".carousel-home{\nbackground-color: $carousel-bg-color;\nheight:auto;\n}\n")
+            
+            ## Jumbotron
             f.write(".jumbotron{\nbackground-color: $jumbotron-bg-color;\ncolor: $jumbotron-text-color;\n}\n")
-            f.write(".jumbotron-button .btn{\nbackground-color: $jumbotron-button-color;\n}\n")
-
-            f.write(".card-title, .card-header {\nheight: 5em;\ntext-align: center;\n line-height: 1.8em;\n}\n")
-            f.write(".card {\nborder: none;\nheight: 20em;\n}\n")
+            f.write(".jumbotron-button .btn{\nbackground-color: $jumbotron-button-color;\ncolor: $jumbotron-text-color;\n}\n")
+            
+            ## Card
+            f.write(".card-header{\nbackground-color: $card-bg-color;\n}\n")
+            f.write(".card-text{\ncolor: $card-font-color;\n}\n")
+            f.write(".card-header {\nheight: auto;\ntext-align: $card-text-align;\n}\n")
+            f.write(".card {\nborder: none;\nheight: $card-height;\n}\n")
+            
+            # Box shadow for cards
             f.write(".card-item {\nbox-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;\n}\n")
-            f.write(".card-img {\nheight: 15em;\nmax-width: 100%;\nobject-fit: cover;\nbackground-color: $white\n}\n")
-            f.write(".card-header{\nbackground-color: $primary;\ncolor: $white;\n}\n")
-            f.write(".card-text{\ncolor: $white;\n}\n")
+            
+            f.write(".card-img {\nheight: $card-img-height;\nmax-width: 100%;\nobject-fit: cover;\nbackground-color: $white\n}\n")        
+            
+
+            ## Custom CSS
+            f.write("\n" + self.css +"\n")
         
 
     
